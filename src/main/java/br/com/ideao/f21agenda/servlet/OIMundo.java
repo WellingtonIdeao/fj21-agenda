0430e204beb07a573187fc9dce33ae0530ecd97b
Wellington Ideão <wellington.ideao@gmail.com>
2024-07-14 20:31:09 -0300

    Mapeamento de uma Servlet para uma url.
    Mapeamento de Servlet via anotação na classe com um nome e
    apenas "uma" url no atributo "value".

--- src/main/java/br/com/ideao/f21agenda/servlet/OIMundo.java
+++ src/main/java/br/com/ideao/f21agenda/servlet/OIMundo.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/oi")
+@WebServlet(name = "primeiraServlet", value = "/oi")
 public class OIMundo extends HttpServlet  {
 
     @Override
