8c19905f7ebaa336d3c808bcffbba0d31ea82845
Wellington Ideão <wellington.ideao@gmail.com>
2024-07-14 19:43:13 -0300

    Primeiro Servlet
    Primeiro servlet OiMundo mapeando para uma url em um arquivo xml.

---
+++ src/main/java/br/com/ideao/f21agenda/servlet/OIMundo.java
@@ -0,0 +1,22 @@
+package br.com.ideao.f21agenda.servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import  javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class OIMundo extends HttpServlet  {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+
+        out.println("<html>");
+        out.println("<body>");
+        out.println("Primeira Servlet");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
